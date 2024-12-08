802c83c77bb667c118b138256b49167e62f615cc
Nelson Soares <nelsonfranciscosoares1987@gmail.com>
2024-12-08 19:07:09 +0000

    -added new package

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"example/hello/printer"
+	"fmt"
+
+	"rsc.io/quote/v4"
+)
 
 func main() {
-	fmt.Println(("Hello, Go Essentials!!!"))
+	fmt.Println("Hello, Go Essentials!!!")
+	fmt.Printf("Valor 10 em hex, 0x%X\n", 10)
+	fmt.Println(quote.Go())
+	printer.Print()
 }
---
+++ printer/printer.go
@@ -0,0 +1,11 @@
+package printer
+
+import "fmt"
+
+func Print() {
+	fmt.Println("Eu sou a função Print")
+}
+
+func publicFunc() {
+	Print()
+}
